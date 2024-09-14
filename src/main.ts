c2e9cfc7db679e375dfca23d218436b6a2582fbf
Jamer Rafael Rodríguez Vergara <jfrodriguez@fgs.co>
2024-09-14 17:12:12 -0500

    car-dealership

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000); // se puede dejar en cualquier puerto ej: 4000
+}
+bootstrap();
